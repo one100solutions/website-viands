7604ed18440a4ff02f536ec6e7f3ae94809577a3
shreyabhat <shreya.bhatk@gmail.com>
2015-06-30 19:26:53 +0530

    otp final

--- js/main.js
+++ js/main.js
@@ -71,16 +71,18 @@ jQuery(document).ready(function($){
 	});
 
 	function login_selected(){
+		$form_otp.removeClass('is-selected');
 		$form_login.addClass('is-selected');
 		$form_signup.removeClass('is-selected');
 		$form_forgot_password.removeClass('is-selected');
 		$tab_login.addClass('selected');
 		$tab_signup.removeClass('selected');
-		$form_otp.removeClass('selected');
+		
 	}
 
 	function signup_selected(){
 		$form_login.removeClass('is-selected');
+		$form_otp.removeClass('is-selected');
 		$form_signup.addClass('is-selected');
 		$form_forgot_password.removeClass('is-selected');
 		$tab_login.removeClass('selected');
